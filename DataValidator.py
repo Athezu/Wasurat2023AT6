08da3f296e31e7943b22875fd56388774d8f7ae8
Athezu <111304714+Athezu@users.noreply.github.com>
2023-12-14 07:22:45 +0100

    Second commit

--- DataValidator.py
+++ DataValidator.py
@@ -1,14 +1,19 @@
 class DataValidator:
-    """A class to validate a list of user input strings, ensuring they are positive integers."""
+    """This class provides functionality to validate a list of strings by checking if each is a positive integer.
+    It is useful in scenarios where user input needs to be validated for numerical processing."""
 
     def validate(self, input_list):
-        """Args:
-            input_list: A list of strings to be validated.
+        """Processes a list of strings, validating each as a positive integer. Non integer and negative values
+        are ignored.
+
+        Args:
+            input_list (str list): User input strings.
 
         Returns:
-            list: A list of valid positive integers."""
+            list of int: Validated positive integers from the input."""
         valid_numbers = []
         for item in input_list:
+            # Validate if each item is a digit and positive
             if item.isdigit() and int(item) > 0:
                 valid_numbers.append(int(item))
         return valid_numbers
